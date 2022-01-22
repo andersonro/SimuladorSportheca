e67b72f1301c337723e78f1b9e0cc916e65a43dc
Anderson <anderson_r_oliveira@hotmail.com>
2022-01-22 16:01:28 -0300

    Desenvolvimento Layout

--- app/src/main/java/br/com/arodevsistemas/simuladorsportheca/ui/DetailActivity.kt
+++ app/src/main/java/br/com/arodevsistemas/simuladorsportheca/ui/DetailActivity.kt
@@ -3,10 +3,17 @@ package br.com.arodevsistemas.simuladorsportheca.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.arodevsistemas.simuladorsportheca.R
+import br.com.arodevsistemas.simuladorsportheca.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityDetailBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
